ea1277f12e453bef58448c32ac6136a3cd4bb612
rchoud19 <rahul_choudhary19@optum.com>
2020-04-10 15:14:17 +0530

    springfox swagger

---
+++ Springfox-Swagger/src/test/java/com/crazzy/spring/documentation/DocumentationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crazzy.spring.documentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
